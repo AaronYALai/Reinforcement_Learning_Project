96a06c52d0ef863a4304c753d1660c70ecf68e53
AaronLai <aaronlaiya@icloud.com>
2016-10-08 02:50:49 +0800

    update agents play

--- agents_play.py
+++ agents_play.py
@@ -2,18 +2,21 @@
 # @Author: aaronlai
 # @Date:   2016-10-08 01:55:41
 # @Last Modified by:   AaronLai
-# @Last Modified time: 2016-10-08 02:36:02
+# @Last Modified time: 2016-10-08 02:50:39
 
 from gomoku_game import initGame, makeMove, getReward, drawGrid, displayGrid
+from agent_utils import load_agent
+
+import numpy as np
 
 
 def agent_play(agent1_name, agent2_name):
     agent1 = load_agent(agent1_name)
     agent2 = load_agent(agent2_name)
-    testAlgo(agent1, agent2)
+    play_game(agent1, agent2)
 
 
-def testAlgo(agent1, agent2, width):
+def play_game(agent1, agent2, width):
     state, available = initGame(width)
     agents = [agent1, agent2]
 
@@ -25,12 +28,13 @@ def testAlgo(agent1, agent2, width):
 
         for actor in range(2):
             step += 1
-            qval = agent1.predict(state.reshape(1, 2 * width**2))
+            qval = agents[actor].predict(state.reshape(1, 2 * width**2))
 
             # policy: choose the move with the max Q value
             action = (np.argmax(qval + available.reshape(1, width**2)))
             action = int(action / width), (action % width)
-            print('Move #: %s; Actor %s, taking action: %s' % (step, actor, action))
+            print('Move #: %s; Actor %s, taking action: %s' %
+                  (step, actor, action))
 
             state, available = makeMove(state, available, action, actor)
             displayGrid(drawGrid(state))
@@ -48,3 +52,9 @@ def testAlgo(agent1, agent2, width):
             break
 
 
+def main():
+    agent_play('agent1.pickle', 'agent2.pickle')
+
+
+if __name__ == '__main__':
+    main()
